6859c421b634443f9036c1eec8e56b8be127381a
New Developer <New@Developer.com>
2020-09-30 14:50:01 +0300

    Added getMessage

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,13 @@
 public class Test {
 
+    private static String getMessage(){
+        return "A message";
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        System.out.println(getMessage());
     }
 
 }
