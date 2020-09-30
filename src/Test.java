a12ad9f1e67cf119cfb1385fe249046430298036
New Developer <New@Developer.com>
2020-09-30 14:50:52 +0300

    Added a hello message

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,16 @@
 public class Test {
 
+    private static String getHello(){
+        return "Hello";
+    }
+
     public static void main(String[] args) {
+        System.out.println(getHello());
+
         System.out.println("Hello world!");
+
+
+
     }
 
 }
